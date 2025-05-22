d3397fa009becf1f0d3076b55c65e3ef7a47fc5c
Raul Unnithan <raul.unnithan03@outlook.com>
2025-05-23 00:52:18 +0100

    Initial commit: Merged topic repos into unified C++ course

---
+++ C-Introduction/Program-with-Variable.cpp
@@ -0,0 +1,26 @@
+//This is a single line comment 
+/*
+This is my multiple lines comments
+
+variables 
+*/
+
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    
+    int a;
+    int a10;
+    int _a;
+    
+    a = 10;
+    a10 = 50; 
+    _a = 600;
+    
+    cout << "a="<<a<<endl;
+    cout << "a10="<<a10<<endl;
+    cout << "_a="<<_a<<endl;
+    
+}
